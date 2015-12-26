d6151992d95c7669c4c1a4de714442e216e895e3
zidong.wang <wangzidongprince@163.com>
2015-12-27 03:57:00 +0800

    sss

--- src/test/java/com/prince/util/HttpUtilTest.java
+++ src/test/java/com/prince/util/HttpUtilTest.java
@@ -10,8 +10,9 @@ public class HttpUtilTest extends TestCase {
 
     public void testHttpUtilGet(){
         HttpUtil httpUtil = HttpUtil.getInstance();
-        String url = "http://zhidao.baidu.com/link?url=lWI8ZRSXNspeUouBkgYTU81dsJ9u-_m9zqilu4AkmJfX6Zgk3pH0HSWsuGgbjWSVoUC1187KDVZ7wPByTu-x8K";
+        String url = "http://www.003zyz.com/listm/index7.html";
         String content = httpUtil.getContentByUrl(url);
         System.out.println(content);
     }
+
 }
